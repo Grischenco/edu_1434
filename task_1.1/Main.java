fa5b14978765e56aa53fc0a460de83b74c6c5295
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-03-27 06:54:57 +0200

    Update Main.java

--- task_1.1/Main.java
+++ task_1.1/Main.java
@@ -16,5 +16,9 @@ name типа String, age типа int и city типа String.
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
+        String name = "Саша";
+        int age = 15;
+        String city = "Киев";
+        
     }
 }
