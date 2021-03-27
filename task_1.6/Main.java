0ac6a7d9845f47537df42c7d26015677f5e2ae7c
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-03-27 07:39:15 +0200

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -1,9 +1,16 @@
 /*
 Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
 */
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Ваш код
+        int a, rez;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        a = reader.nextInt();
+        rez = a + a*0.15;
+        System.out.print("rez = "+rez);
+        
     }
 }
