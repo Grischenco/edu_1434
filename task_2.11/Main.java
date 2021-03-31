93f7e8cc5b666835748682fdbc9f41a7b4cdec33
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-03-31 21:19:21 +0300

    Update Main.java

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -5,6 +5,18 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
+int sum=0, n=0;
+        int[] number = {1,3,5,45,3};
+        for (int i = 0; i < number.length; i++) {
+            if (sum <=10) {
+                n++;
+                sum += number[i];
+            }
+
+        } System.out.println("сумма " + n + " елементов:  " + sum+" > 10");
+
+
+     }}
 
     }
 }
