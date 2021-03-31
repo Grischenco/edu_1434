a07d98c6e80732aac399799ef17414d86f737922
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-03-31 20:41:38 +0300

    Update Main.java

--- task_2.10/Main.java
+++ task_2.10/Main.java
@@ -5,6 +5,11 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+ int [] number = {4,5,6,6,4};
+        for (int i = 0; i < number.length ; i++) {
+                      if (number[i] == number[i+1]  )
+                    System.out.println("yes");
+                      else
+                          System.out.println("noy");
     }
 }
