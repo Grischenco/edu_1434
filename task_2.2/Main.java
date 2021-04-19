f6e02a3ba7b0f4e770fdc90f91abb25ba08cb006
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-04-19 21:20:26 +0300

    Update Main.java

--- task_2.2/Main.java
+++ task_2.2/Main.java
@@ -7,6 +7,17 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+  int i=1,n=1;
+        while(i<=10)
+        {
+            System.out.print(i+" ");
+            while(n<=9)
+            {
+                n++;
+                System.out.print(i*n+" ");
+            }
+            i++;
+            n=1;
+            System.out.println("");
     }
 }
