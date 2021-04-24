6e08680fae66c1f8de00047d282d189b3f843da6
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-04-24 19:25:31 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -5,6 +5,11 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+ int[]arr=new int[]{2,3,4,5};
+        int op = 1;
+        for (int i = 0; i < arr.length; i++) {
+            op *=arr[i];
+        }
+        System.out.println(op);
     }
 }
