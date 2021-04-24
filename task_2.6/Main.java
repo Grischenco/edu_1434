55891d75bd8716f79154c0335b07128815e752fd
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-04-24 17:44:16 +0300

    Update Main.java

--- task_2.6/Main.java
+++ task_2.6/Main.java
@@ -5,6 +5,16 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+ Scanner in = new Scanner(System.in);
+       float a, b;
+        System.out.println("a= ");
+        a = in.nextInt();
+        System.out.println("b= ");
+        b = in.nextInt();
+        if (a % b != 0){
+                     System.out.println("rez = " + a/b);
+        }
+        else
+            System.out.println("yes");
     }
 }
