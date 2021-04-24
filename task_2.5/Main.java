1e6b9ac7a4404cdbceff4eba264947928dccdeb3
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-04-24 16:54:23 +0300

    Update Main.java

--- task_2.5/Main.java
+++ task_2.5/Main.java
@@ -5,6 +5,15 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
+ Scanner in =new Scanner(System.in);
+       int a;
+        System.out.println("a= ");
+        a = in.nextInt();
+ while (a !=10){
+     System.out.println("Not, enter a= ");
+     a = in.nextInt();
+ }
+        System.out.println(" Yes, esc");
 
     }
 }
