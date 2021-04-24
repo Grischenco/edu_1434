900db29071f99c375147f151da341f5fb28d5400
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-04-24 20:33:37 +0300

    Update Main.java

--- task_2.9/Main.java
+++ task_2.9/Main.java
@@ -5,6 +5,17 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+ int[]arr=new int[10];
+       int n=0, x=1, y=10;
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = (int) (Math.random() * 10);
+            for (int i :arr) {
+                System.out.print(i+" ");
+            } System.out.println();
+       for (int i = 0; i < arr.length; i++){
+           if(arr[i]==5)
+               n++;
+       //else System.out.println("noy");
+    } System.out.println("число 5 найдено "+n +" разa");
     }
 }
