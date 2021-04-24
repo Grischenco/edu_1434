9f806ea28c9afa2c5e68d7492e0d66f99a2ba335
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-04-24 16:19:04 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -5,6 +5,29 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
+ 
+        Scanner in =new Scanner(System.in);
+ int vklad;
+ int prosent, p;
+ int spok;
+ double sum=0;
+        System.out.println("введите вклад:");
+        vklad=in.nextInt();
+        System.out.println("введите процент:");
+        prosent=in.nextInt();
+        System.out.println("введите срок:");
+        spok=in.nextInt();
+        prosent = (vklad*prosent)/100;
+        p=vklad+prosent;
 
+        for (int i = 1; i <=spok ; i++) {
+            System.out.println("процент за: " + i + " - год = " + (prosent*i));
+            p=prosent*i;
+             sum= vklad+p;
+            System.out.println("Всего: " + sum);
+        }
+
+            }
+        }
     }
-}
+
