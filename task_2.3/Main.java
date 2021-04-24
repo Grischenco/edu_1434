dc6c5bd671c59e0b864125e1dd8fd8f21c0532b6
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-04-24 09:38:34 +0300

    Update Main.java

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -16,6 +16,28 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
+public class Yrok_5 {
+    public static void main(String[] args) {
+        //ПЕРЕМЕННЫЕ
+int i,j;
+        //создаем массив
+int [][] nums = new int[10][];
+//цыкл для создания триугольного массива
+        for ( i = 0; i < nums.length; i++) {
+            //создание строки в массиве
+            nums[i] = new int[i + 1];}
+        //заполняем двухмерный массив
+        for ( i = 0; i < nums.length; i++) {
+                for ( j = 0; j <nums[i].length ; j++) {
+                    //вводи значение елемента массива
+                    nums[i][j] = 8;
+                    //выводим на екран
+                    System.out.print(nums[i][j] );
+                }System.out.println();
 
-    }
-}
+                }
+
+            }
+        }
+    
+
