32e033a348e75a493fdcfc1c7c14928d0a6104bd
Grischenco <81438686+Grischenco@users.noreply.github.com>
2021-04-25 18:26:20 +0300

    Update Main.java

--- task_3.4/Main.java
+++ task_3.4/Main.java
@@ -7,6 +7,15 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+Scanner in = new Scanner(System.in);
+        System.out.println("Enter number: a, b ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        if (n(a,b)>10) System.out.println(true);
+        else System.out.println(false);
+    }
+    static int n(int a, int b) {
+      int sum =a+b;
+      return sum;
     }
 }
